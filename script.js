5a1ced3c618b49179e9f21ee48bfaa6d9c15b76e
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2025-04-29 07:55:16 +1000

    finish UI and js

---
+++ script.js
@@ -0,0 +1,7 @@
+let popup = document.getElementById("popup");
+function showpopup() {
+    popup.classList.add ("open-popup");
+}
+function hidepopup() {
+    popup.classList.remove ("open-popup");
+}
